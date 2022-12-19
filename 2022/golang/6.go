d6ba2c4e3774320e776192a5972bff8ce761cf1e
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2022-12-19 07:42:35 -0300

    solution 6

--- 2022/golang/6.go
+++ 2022/golang/6.go
@@ -6,19 +6,19 @@ import (
 	"strings"
 )
 
-func get_packet(content string, marker_len int) {
+func get_first_packet_where_all_chars_are_different(content string, packet_len int) {
 	for _, line := range strings.Split(content, "\n") {
 		buffer := strings.TrimSpace(line)
-		for i := marker_len - 1; i < len(buffer); i++ {
+		for i := packet_len - 1; i < len(buffer); i++ {
 			chars := make(map[byte]bool)
-			for j := i; j > i-marker_len; j-- {
+			for j := i; j > i-packet_len; j-- {
 				present, _ := chars[buffer[j]]
 				if present {
 					break
 				}
 				chars[buffer[j]] = true
 			}
-			if len(chars) == marker_len {
+			if len(chars) == packet_len {
 				fmt.Println(i + 1)
 				break
 			}
@@ -31,5 +31,5 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	get_packet(string(bytes), 14)
+	get_first_packet_where_all_chars_are_different(string(bytes), 14)
 }
