95bd35064c1a3175d5aa281f814e613c71edff18
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2023-12-05 12:52:11 -0300

    part 4 python

--- 2023/python/aoc4/aoc4.py
+++ 2023/python/aoc4/aoc4.py
@@ -2,19 +2,13 @@
 from dataclasses import dataclass
 
 
-def points(winning_numbers):
-    if winning_numbers == 0:
-        return 0
-    return 2 ** (winning_numbers - 1)
-
-
 @dataclass
 class Card:
     name: str
     winning_numbers: list[int]
     my_numbers: list[int]
 
-    def my_winning_numbers(self):
+    def match_numbers(self):
         count = 0
         for n in self.my_numbers:
             if n in self.winning_numbers:
@@ -22,13 +16,13 @@ class Card:
         return count
 
     def points(self):
-        n = self.my_winning_numbers()
+        n = self.match_numbers()
         if n == 0:
             return 0
         return 2 ** (n - 1)
 
 
-def parse_input(input: str):
+def parse_input(input: str) -> list[Card]:
     cards = []
     with open(input) as f:
         for line in f.readlines():
@@ -43,10 +37,18 @@ def parse_input(input: str):
             )
     return cards
 
-def part1(input):
-    sum = 0
-    for card in parse_input(input):
-        sum += card.points()
-    return sum
 
-part1("/home/humberto/projects/aoc/2023/04.input")
+def part1(input):
+    return sum(card.points() for card in parse_input(input))
+
+
+def part2(input):
+    cards = [{"matches": n.match_numbers(), "count": 1} for n in parse_input(input)]
+    for i, m in enumerate(cards):
+        matches = m["matches"]
+        count = m["count"]
+        # para as matches cards seguintes ganha uma cópia de cada
+        # ex: se a card 1 tem 4 matches, adiciona uma cópia para a card 2 a 5
+        for j in range(matches):
+            cards[i + j + 1]["count"] += count
+    return sum(card["count"] for card in cards)
---
+++ 2023/python/aoc4/aoc4_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from aoc4 import part1, part2
+
+INPUT = "/home/humberto/projects/aoc/2023/04.input"
+
+def test_part1():
+    assert part1(INPUT) == 20667
+
+def test_part2():
+    assert part2(INPUT) == 5833065
