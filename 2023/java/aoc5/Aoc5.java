1d8782abefc5f2fb68f7afda0f838cca38cd8f5a
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2023-12-06 22:01:00 -0300

    aoc 5 java part1

---
+++ 2023/java/aoc5/Aoc5.java
@@ -0,0 +1,91 @@
+
+/**
+ * Aoc5
+ */
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Aoc5 {
+
+    record Range(int src, int dest, int len) {
+    }
+
+    record Mapping(String src, String dest, List<Range> ranges) {
+        static Mapping parse(String section) {
+            var parts = section.split(":");
+            var header = parts[0].replace(" map", "").split("-to-");
+            var src = header[0];
+            var dest = header[1];
+            var ranges = Stream.of(parts[1].trim().split("\n"))
+                    .map(line -> {
+                        var numbers = line.split(" ");
+                        return new Range(Integer.parseInt(numbers[0]),
+                                Integer.parseInt(numbers[1]),
+                                Integer.parseInt(numbers[2]));
+                    }).toList();
+            return new Mapping(src, dest, ranges);
+        }
+
+        int convert(int number) {
+            return ranges.stream().filter(r -> number >= r.src && number <= r.src + r.len)
+                    .map(r -> number + (r.dest - r.src))
+                    .findFirst()
+                    .orElse(number);
+        }
+    }
+
+    record Almanac(List<Integer> seeds, List<Mapping> mappings) {
+        static Almanac parse(String fileName) throws IOException {
+            var text = Files.readString(Path.of(fileName));
+            var sections = text.split("\n\n");
+            var seeds = new ArrayList<Integer>();
+            for (String seed : sections[0].split(":")[1].trim().split(" ")) {
+                seeds.add(Integer.parseInt(seed.trim()));
+            }
+            var mappings = Stream.of(sections).skip(1).map(section -> Mapping.parse(section))
+                    .toList();
+            return new Almanac(seeds, mappings);
+        }
+
+        Mapping getMapping(String src, String dest) {
+            return mappings.stream().filter(mapping -> mapping.src.equals(src) && mapping.dest.equals(dest))
+                    .findFirst()
+                    .orElseThrow(() -> new RuntimeException("Mapping not found: " + src + " " + "dst"));
+        }
+    }
+
+    static List<String> categories = List.of(
+            "seed",
+            "soil",
+            "fertilizer",
+            "water",
+            "light",
+            "temperature",
+            "humidity",
+            "location");
+
+    static void part1(Almanac almanac) {
+        var lowest = 1e9;
+        for (int seed : almanac.seeds) {
+            var location = seed;
+            for (int i = 0; i < categories.size() - 1; i++) {
+                var mapping = almanac.getMapping(categories.get(i), categories.get(i + 1));
+                location = mapping.convert(location);
+            }
+            lowest = Math.min(lowest, location);
+        }
+        System.out.println(lowest);
+    }
+
+    public static void main(String... args) throws IOException {
+        var almanac = Almanac.parse("/home/humberto/projects/aoc/2023/05.example");
+        part1(almanac);
+    }
+
+}
