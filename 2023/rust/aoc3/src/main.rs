53e6acf7ac407a9b5aea8a9b32282ed72a7e7ec7
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2023-12-13 14:54:41 -0300

    rust aoc 3 part 1, wrong answer

---
+++ 2023/rust/aoc3/src/main.rs
@@ -0,0 +1,77 @@
+use std::fs::{self};
+
+fn cartesian_product<T: Clone>(vec1: &[T], vec2: &[T]) -> Vec<(T, T)> {
+    vec1.iter()
+        .flat_map(|x| vec2.iter().map(move |y| (x.to_owned(), y.to_owned())))
+        .collect()
+}
+
+// return if number is adjacent to a symbol
+fn is_part_number(number: &[(usize, usize)], symbol_positions: &[(usize, usize)]) -> bool {
+    let adjacent_neighbors =
+        cartesian_product(&[-1isize, 0isize, 1isize], &[-1isize, 0isize, 1isize]);
+    for (row, column) in number {
+        for (x, y) in &adjacent_neighbors {
+            let i = x + *row as isize;
+            let j = y + *column as isize;
+            if i < 0 || j < 0 {
+                continue;
+            }
+            if symbol_positions.contains(&(i as usize, j as usize)) {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn get_char_at_pos(text: &str, pos: &(usize, usize), row_len: usize) -> char {
+    text.chars().nth(pos.1 + row_len * pos.0).unwrap()
+}
+
+fn get_number(number: &[(usize, usize)], input_no_newlines: &str, row_len: usize) -> usize {
+    let value = number
+        .iter()
+        .map(|pos| get_char_at_pos(input_no_newlines, pos, row_len))
+        .collect::<String>();
+    value
+        .parse::<usize>()
+        .expect(&("not a number ".to_owned() + &value))
+}
+fn main() {
+    let input_string = fs::read_to_string("/home/humberto/projects/aoc/2023/03.input").unwrap();
+    let mut consecutive_digit_positions: Vec<Vec<(usize, usize)>> = Vec::new();
+    let mut current_consecutive_positions: Vec<(usize, usize)> = Vec::new();
+    let mut symbol_positions: Vec<(usize, usize)> = Vec::new();
+    let row_len = input_string.find('\n').unwrap();
+    let input_no_newlines = input_string.replace('\n', "");
+
+    for (i, c) in input_no_newlines.trim().char_indices() {
+        let row = i / row_len;
+        let column = i % row_len;
+        // capture digits positions
+        if c.is_ascii_digit() {
+            current_consecutive_positions.push((row, column));
+        } else if !current_consecutive_positions.is_empty() {
+            consecutive_digit_positions.push(current_consecutive_positions.clone());
+            current_consecutive_positions.clear();
+        }
+
+        // capture symbol positions
+        if c != '.' && !c.is_ascii_digit() && !c.is_whitespace() {
+            symbol_positions.push((row, column));
+        }
+    }
+
+    // Check if there are consecutive digits at the end of the string
+    if !current_consecutive_positions.is_empty() {
+        consecutive_digit_positions.push(current_consecutive_positions);
+    }
+
+    let sum: usize = consecutive_digit_positions
+        .iter()
+        .filter(|number| is_part_number(number, &symbol_positions))
+        .map(|number| get_number(number, &input_no_newlines, row_len))
+        .sum();
+    println!("sum = {}", sum);
+}
