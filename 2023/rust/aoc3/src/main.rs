cfbcf9fe0e826b49e6b8148c87621b3edba67070
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2023-12-19 10:03:30 -0300

    changes from fedora on ter 19 dez 2023 10:03:30 -03

--- 2023/rust/aoc3/src/main.rs
+++ 2023/rust/aoc3/src/main.rs
@@ -1,7 +1,7 @@
 use regex::bytes::Regex;
 use std::fs::{self};
 
-fn part1_() -> usize {
+fn part1() -> usize {
     let mut result = 0;
     let input = fs::read_to_string("/home/humberto/projects/aoc/2023/03.input").unwrap();
     let lines = input.lines().collect::<Vec<&str>>();
@@ -9,70 +9,56 @@ fn part1_() -> usize {
 
     //for each line test if number is surrounded by a symbol, if its add it to the result
     lines.iter().enumerate().for_each(|(i, line)| {
-        let number = line.replace('.', " ");
-        re.find_iter(number.as_bytes()).for_each(|m| {
+        // let number = line.replace('.', " ");
+        re.find_iter(line.as_bytes()).for_each(|m| {
             let number_value = String::from_utf8_lossy(m.as_bytes())
                 .parse::<usize>()
                 .unwrap();
             for j in m.start()..m.end() {
-                'outer: for x in -1isize..=1 {
-                    for y in -1isize..=1 {
-                        let row_num = if x < 0 { i.wrapping_sub(-x) } else { i + x };
-                        let col_num = if y < 0 { j.wrapping_sub(-y) } else { j + y };
-                        let c = lines
-                            .get(row_num)
-                            .and_then(|row| row.chars().nth(col_num))
-                            .unwrap();
-                        if !c.is_ascii_digit() && c != '.' {
-                            result += number_value;
-                            break 'outer;
-                        }
-                    }
-                }
                 // Construct the surrounding array
-                // let surrounding = vec![
-                //     lines
-                //         .get(i.wrapping_sub(1))
-                //         .and_then(|row| row.chars().nth(j.wrapping_sub(1)))
-                //         .unwrap_or('.'),
-                //     lines
-                //         .get(i.wrapping_sub(1))
-                //         .and_then(|row| row.chars().nth(j))
-                //         .unwrap_or('.'),
-                //     lines
-                //         .get(i.wrapping_sub(1))
-                //         .and_then(|row| row.chars().nth(j + 1))
-                //         .unwrap_or('.'),
-                //     lines
-                //         .get(i)
-                //         .and_then(|row| row.chars().nth(j.wrapping_sub(1)))
-                //         .unwrap_or('.'),
-                //     lines
-                //         .get(i)
-                //         .and_then(|row| row.chars().nth(j))
-                //         .unwrap_or('.'),
-                //     lines
-                //         .get(i)
-                //         .and_then(|row| row.chars().nth(j + 1))
-                //         .unwrap_or('.'),
-                //     lines
-                //         .get(i + 1)
-                //         .and_then(|row| row.chars().nth(j.wrapping_sub(1)))
-                //         .unwrap_or('.'),
-                //     lines
-                //         .get(i + 1)
-                //         .and_then(|row| row.chars().nth(j))
-                //         .unwrap_or('.'),
-                //     lines
-                //         .get(i + 1)
-                //         .and_then(|row| row.chars().nth(j + 1))
-                //         .unwrap_or('.'),
-                // ];
-                // // test if the surrounding has a symbol
-                // if surrounding.iter().any(|c| !c.is_ascii_digit() && *c != '.') {
-                //     result += number_value;
-                //     break;
-                // }
+                let surrounding = vec![
+                    lines
+                        .get(i.wrapping_sub(1))
+                        .and_then(|row| row.chars().nth(j.wrapping_sub(1)))
+                        .unwrap_or('.'),
+                    lines
+                        .get(i.wrapping_sub(1))
+                        .and_then(|row| row.chars().nth(j))
+                        .unwrap_or('.'),
+                    lines
+                        .get(i.wrapping_sub(1))
+                        .and_then(|row| row.chars().nth(j + 1))
+                        .unwrap_or('.'),
+                    lines
+                        .get(i)
+                        .and_then(|row| row.chars().nth(j.wrapping_sub(1)))
+                        .unwrap_or('.'),
+                    lines
+                        .get(i)
+                        .and_then(|row| row.chars().nth(j))
+                        .unwrap_or('.'),
+                    lines
+                        .get(i)
+                        .and_then(|row| row.chars().nth(j + 1))
+                        .unwrap_or('.'),
+                    lines
+                        .get(i + 1)
+                        .and_then(|row| row.chars().nth(j.wrapping_sub(1)))
+                        .unwrap_or('.'),
+                    lines
+                        .get(i + 1)
+                        .and_then(|row| row.chars().nth(j))
+                        .unwrap_or('.'),
+                    lines
+                        .get(i + 1)
+                        .and_then(|row| row.chars().nth(j + 1))
+                        .unwrap_or('.'),
+                ];
+                // test if the surrounding has a symbol
+                if surrounding.iter().any(|c| !c.is_ascii_digit() && *c != '.') {
+                    result += number_value;
+                    break;
+                }
             }
         });
     });
@@ -80,7 +66,7 @@ fn part1_() -> usize {
 }
 
 fn main() {
-    println!("{}", part1_());
+    println!("{}", part1());
 }
 
 #[cfg(test)]
@@ -89,6 +75,6 @@ mod tests {
 
     #[test]
     fn part1_test() {
-        assert_eq!(556367, part1_());
+        assert_eq!(556367, part1());
     }
 }
