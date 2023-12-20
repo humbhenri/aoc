fe26b1b591a3db427849bf9a2fba6c121f39e3fe
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2023-12-20 15:21:41 -0300

    day 9 part 1 done

--- 2023/rust/aoc9/src/main.rs
+++ 2023/rust/aoc9/src/main.rs
@@ -1,15 +1,13 @@
-fn main() {
-    println!("Hello, world!");
-}
+use std::fs;
 
-fn next_value(seq: &[u32]) -> u32 {
+fn next_value(seq: &[i32]) -> i32 {
     if seq.iter().all(|&x| x == 0) {
         return 0;
     }
     seq.last().unwrap() + next_value(&sequence_below(seq))
 }
 
-fn sequence_below(seq: &[u32]) -> Vec<u32> {
+fn sequence_below(seq: &[i32]) -> Vec<i32> {
     if seq.iter().all(|&x| x == 0) {
         return Vec::from(seq);
     }
@@ -17,7 +15,25 @@ fn sequence_below(seq: &[u32]) -> Vec<u32> {
     seq.iter()
         .zip(rest.iter())
         .map(|(a, b)| (b - a))
-        .collect::<Vec<u32>>()
+        .collect::<Vec<i32>>()
+}
+
+fn parse_input(path: &str) -> Vec<Vec<i32>> {
+    fs::read_to_string(path)
+        .unwrap()
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .collect()
+}
+
+fn main() {
+    let input = "/home/humberto/projects/aoc/2023/09.input";
+    let part1: i32 = parse_input(input).iter().map(|seq| next_value(seq)).sum();
+    println!("Part 1 = {}", part1);
 }
 
 #[cfg(test)]
@@ -36,5 +52,6 @@ mod tests {
         assert_eq!(18, next_value(&[0, 3, 6, 9, 12, 15]));
         assert_eq!(0, next_value(&[0, 0, 0]));
         assert_eq!(28, next_value(&[1, 3, 6, 10, 15, 21]));
+        assert_eq!(68, next_value(&[10, 13, 16, 21, 30, 45]));
     }
 }
